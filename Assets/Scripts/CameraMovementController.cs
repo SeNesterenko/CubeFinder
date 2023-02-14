33cac7cbfc14c18f05916d8c6f74ddce9cc81b3f
snesterenko <snesterenko@dtln.ru>
2023-02-14 10:36:27 +0300

    Last fixes for Parallax Effect

--- Assets/Scripts/CameraMovementController.cs
+++ Assets/Scripts/CameraMovementController.cs
@@ -4,17 +4,11 @@ public class CameraMovementController : MonoBehaviour
 {
     [SerializeField] private float _speed = 2f;
     
-    private Camera _camera;
     private Vector3 _direction = Vector3.right;
-
-    private void Awake()
-    {
-        _camera = Camera.main;
-    }
 
     private void Update()
     {
         var distance = _speed * Time.deltaTime;
-        transform.position= _direction * distance;
+        transform.position += _direction * distance;
     }
 }
